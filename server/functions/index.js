b0dd45182e66e3a1b7ee7b39974331177c3ad31a
Slavi Petrov <slavi@devlabs.bg>
2018-02-28 11:14:46 +0200

    Updated Firebase Cloud Functions

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,40 +1,12 @@
+'use strict';
+
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-exports.sendNotification = functions.database.ref('/notifications/invitations/{pushId}')
-    .onWrite(event => {
-        const invitation = event.data.current.val();
-        const senderUid = message.from;
-        const receiverUid = message.to;
-        const promises = [];
+exports.sendInvitationNotification = functions.database.ref('users/{user_id}/invitations/{invitation_id}').onWrite(event => {
+    const user_id = event.params.user_id;
+    const invitation_id = event.params.invitation_id;
 
-        // The sender and the receiver are the same person - don't send a notification
-        if (senderUid == receiverUid) {
-            promises.push(event.data.current.ref.remove());
-            return Promise.all(promises);
-        }
-
-        const getInstanceIdPromise = admin.database().ref('/users/${receiverUid}/instanceId').once('value');
-        const getReceiverUidPromise = admin.auth().getUser(receiverUid);
-
-        return Promise.all([getInstanceIdPromise, getReceiverUidPromise]).then(results => {
-            const instanceId = results[0].val();
-            const receiver = results[1];
-            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
-
-            const payload = {
-                notification: {
-                    title: receiver.displayName,
-                    body: message.body,
-                    icon: receiver.photoURL
-                }
-            };
-
-            admin.messaging().sendToDevice(instanceId, payload)
-                .then(function (response) {
-                    console.log("Successfully send message: ", response);
-                })
-                .catch(function (error) {
-                    console.log("Error sending message: ", error);
-                });
-        });
-    });
+    console.log("User ID: " + user_id + " |Invitation ID: " + invitation_id)
+});
